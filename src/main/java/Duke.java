df75552cb0c09f9e24f2bab5a49f0ce32d073e64
Jarren San <79039461+jarrensan@users.noreply.github.com>
2026-01-22 12:41:08 +0800

    Add support for list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
@@ -8,12 +9,22 @@ public class Duke {
         String h_line = "\n____________________________________________________________\n";
         String greeting = " Hello! I'm JellyBot\n What can I do for you?";
         String bye = " Bye. Hope to see you again soon!";
+        ArrayList<String> stringList = new ArrayList<>();
 
         System.out.println(h_line + greeting + h_line);
 
         String input = br.readLine();
         while (!input.equals("bye")) {
-            System.out.println(h_line + " " + input + h_line);
+            if (input.equals("list")) {
+                String list_out = "";
+                for (int i = 0; i < stringList.size(); i++) {
+                    list_out += (i+1 + ". " + stringList.get(i) + "\n");
+                }
+                System.out.println(h_line + list_out + h_line);
+            } else {
+                stringList.add(input);
+                System.out.println(h_line + " " + input + h_line);
+            }
             input = br.readLine();
         }
 
