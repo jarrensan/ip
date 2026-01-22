7576bfbcd17d5cc5a997a81e95f5239e04a68772
Jarren San <79039461+jarrensan@users.noreply.github.com>
2026-01-22 12:30:12 +0800

    Add support for echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String h_line = "\n____________________________________________________________\n";
-        String gretting = " Hello! I'm JellyBot\n What can I do for you?";
+        String greeting = " Hello! I'm JellyBot\n What can I do for you?";
         String bye = " Bye. Hope to see you again soon!";
 
-        System.out.println(h_line + gretting + h_line + bye + h_line);
+        System.out.println(h_line + greeting + h_line);
+
+        String input = br.readLine();
+        while (!input.equals("bye")) {
+            System.out.println(h_line + " " + input + h_line);
+            input = br.readLine();
+        }
+
+        System.out.println(h_line + bye + h_line);
     }
 }
