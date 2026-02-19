6c24090de7a66098d8ab3bc36d6005bb2d7dee45
Jarren San <79039461+jarrensan@users.noreply.github.com>
2026-02-20 01:11:28 +0800

    Update User Guide

--- docs/README.md
+++ docs/README.md
@@ -1,30 +1,131 @@
 # Jelly User Guide
 
-// Update the title above to match the actual product name
+![](Screenshot.png)
+Jelly tasklist
 
-// Product screenshot goes here
-
-// Product intro goes here
 
 ## Adding deadlines
+Add deadline tasks
 
-// Describe the action and its outcome.
+### Usage: 
+`deadline <description> /by <dd/MMM/yyyy>`
 
-// Give examples of usage
+### Example: 
+`deadline iP task /by 01/Jan/2025`
 
-Example: `keyword (optional arguments)`
-
-// A description of the expected outcome goes here
-
+### Expected Output:
 ```
-expected output
+Blob. I've added this task:
+[D][] iP task (by: 01/Jan/2025)
+Now you have 1 task in the list blob.
 ```
 
-## Feature ABC
+## Adding events
+Add event tasks
 
-// Feature details
+### Usage:
+`event <description> /from <dd/MMM/yyyy> /to <dd/MMM/yyyy>`
 
+### Example:
+`event tP project /from 01/Jan/2025 /to 02/Feb/2026`
 
-## Feature XYZ
+### Expected Output:
+```
+Blob. I've added this task:
+[D][] tP project (from: 01/Jan/2025 to: 02/Feb/2026)
+Now you have 2 task in the list blob.
+```
 
-// Feature details
+## Adding todos
+Add todo tasks
+
+### Usage:
+`todo <description>`
+
+### Example:
+`todo submit iP`
+
+### Expected Output:
+```
+Blob. I've added this task:
+[D][] submit iP
+Now you have 3 task in the list blob.
+```
+
+## Listing all tasks
+List all existing tasks
+
+### Usage:
+`list`
+
+### Example:
+`list`
+
+### Expected Output:
+```
+Blob! Here are the tasks in your list:
+1. [D][] iP task (by: 01/Jan/2025)
+2. [D][] tP project (from: 01/Jan/2025 to: 02/Feb/2026)
+3. [D][] submit iP
+```
+
+## Marking tasks
+Mark an existing task
+
+### Usage:
+`mark <task number>`
+
+### Example:
+`mark 1`
+
+### Expected Output:
+```
+Bloob bloob! I've marked this task as done:
+[D][X] iP task (by: 01/Jan/2025)
+```
+
+## Unmarking tasks
+Unmark an existing task
+
+### Usage:
+`unmark <task number>`
+
+### Example:
+`unmark 1`
+
+### Expected Output:
+```
+Blob..., I've marked this task as not done yet:
+[D][] iP task (by: 01/Jan/2025)
+```
+
+## Updating tasks
+Update description of an existing task
+
+### Usage:
+`update <task number> <description>`
+
+### Example:
+`update 1 iP homework`
+
+### Expected Output:
+```
+Blob, I've updated this task to:
+[T][] iP homework (by: 01/Jan/2025)
+```
+
+## Deleting tasks
+Delete an existing task
+
+### Usage:
+`delete <task number>`
+
+### Example:
+`delete 1`
+
+### Expected Output:
+```
+Blob blob. I've removed this task:
+[D][] iP homework (by: 01/Jan/2025)
+Now you have 2 task in the list blob.
+```
--- src/main/java/jelly/ui/Ui.java
+++ src/main/java/jelly/ui/Ui.java
@@ -7,25 +7,25 @@ public class Ui {
     private static final String h_line = "\n____________________________________________________________\n";
 
     public String showGreeting() {
-        return h_line + " Hello! I'm JellyBot\n What can I do for you?" + h_line;
+        return h_line + " Blob! I'm Jelly blob!\n What can I do for you blob?" + h_line;
     }
 
     public String showBye() {
-        return h_line + " Bye. Hope to see you again soon!" + h_line;
+        return h_line + " Bleoob. Hope to see you again soon!" + h_line;
     }
 
     public String showMarkTask(Task task) {
-        return h_line + "Nice! I've marked this task as done: \n" + task + h_line;
+        return h_line + "Bloob bloob! I've marked this task as done: \n" + task + h_line;
     }
 
     public String showUnmarkTask(Task task) {
-        return h_line + "OK, I've marked this task as not done yet: \n" + task + h_line;
+        return h_line + "Blob..., I've marked this task as not done yet: \n" + task + h_line;
     }
 
     public String showAddTask(Task task, int taskSize) {
         return h_line
-                + " Got it. I've added this task:\n   " + task
-                + "\n Now you have " + taskSize + " in the list." + h_line;
+                + " Blob. I've added this task:\n   " + task
+                + "\n Now you have " + taskSize + " task in the list blob." + h_line;
     }
 
     public String showError(Exception e) {
@@ -34,25 +34,25 @@ public class Ui {
 
     public String showList(TaskList taskList) {
         if (taskList.isEmpty()) {
-            return h_line + " Empty List! Add some tasks first!" + h_line;
+            return h_line + " Bubu... Empty List! Add some tasks first!" + h_line;
         }
-        return h_line + " Here are the tasks in your list: " + taskList + h_line;
+        return h_line + " Blob! Here are the tasks in your list: " + taskList + h_line;
     }
 
     public String showFindList(TaskList taskList) {
         if (taskList.isEmpty()) {
-            return h_line + " No matching tasks found! Please try again." + h_line;
+            return h_line + " Blu... No matching tasks found! Please try again." + h_line;
         }
-        return h_line + " Here are the matching tasks in your list: " + taskList + h_line;
+        return h_line + " Blim! Here are the matching tasks in your list: " + taskList + h_line;
     }
 
     public String showUpdateTask(Task task) {
-        return h_line + "OK, I've updated this task to: \n" + task + h_line;
+        return h_line + "Blob, I've updated this task to: \n" + task + h_line;
     }
 
     public String showDeleteTask(Task task, int taskSize) {
         return h_line
-                + "Noted. I've removed this task:\n   " + task
+                + "Blob blob. I've removed this task:\n   " + task
                 + "\n Now you have " + taskSize + " in the list." + h_line;
     }
 }
